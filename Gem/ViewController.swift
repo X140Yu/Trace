638a9333a73a0fb809df96715455dface9fc6e79
X140Yu <zhaoxinyu1994@gmail.com>
2017-02-25 11:31:00 +0800

    Initial Commit

---
+++ Gem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gem
+//
+//  Created by X140Yu on 2/24/17.
+//  Copyright © 2017 X140Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
